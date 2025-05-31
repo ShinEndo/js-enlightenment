f686e06f218226d1ce1154e5a3e39fa44e598dd9
遠藤慎 <end3mame@gmail.com>
2025-05-31 16:14:30 +0900

    feat: 継承チェーンを生成する

---
+++ chapter08/8.12.js
@@ -0,0 +1,16 @@
+// **************************
+// 8.12 継承チェーンを生成する
+// **************************
+
+const Person = function(){ this.bar = 'bar'; }
+Person.prototype.foo = 'foo';
+
+const Chef = function(){ this.goo = 'goo'; }
+Chef.prototype = new Person();
+const cody = new Chef();
+
+console.log(cody.foo);
+console.log(cody.bar);
+console.log(cody.goo);
+
+
