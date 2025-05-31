098d45e6c4d6f84f81fafc4bf6c9f4655855d29c
遠藤慎 <end3mame@gmail.com>
2025-05-31 09:42:28 +0900

    feat: コンストラクタ関数の作成

--- chapter01/1.1.js
+++ chapter01/1.1.js
@@ -20,4 +20,14 @@ console.log(myObject);
 const myString = new String('foo');
 console.log(myString);
 
+const Person = function(living,age,gender){
+	this.living = living;
+	this.age = age;
+	this.gender = gender;
+	this.getGender = function() {return this.gender;}
+};
+
+const cody_01 = new Person(true,33,'male');
+console.log(cody_01);
+
 
